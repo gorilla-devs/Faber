d3642bfb7dfd163caf197ba91584f274e3cc0474
Jacob Sapoznikow <94275204+RedstoneWizard08@users.noreply.github.com>
2022-06-15 19:40:58 -0700

    Fix requests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+pub mod maven;
+pub mod util;
+
+pub use maven::*;
+
+pub async fn get_fabric_versions() -> MavenVersionResult {
+    let resp = util::get(
+        "https://maven.fabricmc.net/net/fabricmc/fabric-loader/maven-metadata.xml".to_string(),
+    );
+    let raw = xmltojson::to_json(&resp).expect("Something went wrong.");
+    let serialized = serde_json::to_string(&raw).unwrap();
+    let des: RawMavenVersionResult = serde_json::from_str(&serialized).unwrap();
+    return des.metadata;
 }
